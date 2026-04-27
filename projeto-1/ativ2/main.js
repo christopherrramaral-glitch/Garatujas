8339c97950815d503c5fc290fecb4929bf76029b
Amaralkslsk <christopher.rr.amaral@gmail.com>
2026-04-27 14:53:12 +0000

    minima mudança

--- projeto-1/ativ2/main.js
+++ projeto-1/ativ2/main.js
@@ -1,4 +1,4 @@
-const prevBtn = document.querySelector("#prev-btn");
+const prevBtn = document.querySelector("#prev-btn"); //
 const nextBtn = document.querySelector("#next-btn");
 const book = document.querySelector("#book");
 
